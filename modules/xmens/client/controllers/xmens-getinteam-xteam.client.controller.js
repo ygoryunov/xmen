57c11e1973b80e58957c99617ebd9813dcfd6b4a
Yuriy Goryunov <ygoryunov@dataart.com>
2016-08-22 21:23:50 +0200

    return controller

---
+++ modules/xmens/client/controllers/xmens-getinteam-xteam.client.controller.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('xmens')
+    .controller('XmensGetinteamXteamController', XmensGetinteamXteamController);
+
+  XmensGetinteamXteamController.$inject = ['$scope'];
+
+  function XmensGetinteamXteamController($scope) {
+    var vm = this;
+
+    // Xmens getinteam xteam controller logic
+    // ...
+
+    init();
+
+    function init() {
+    }
+  }
+})();
